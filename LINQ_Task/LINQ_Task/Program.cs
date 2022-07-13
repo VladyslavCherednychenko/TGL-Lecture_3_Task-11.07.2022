8630a6e190f89bd270b017a5c059052cfb242236
Vladyslav Cherednychenko <vladislav.cherednychenko@gmail.com>
2022-07-13 21:37:58 +0300

    Complete task of implementation factorial function and Fibonacci sequence using LINQ

--- LINQ_Task/LINQ_Task/Program.cs
+++ LINQ_Task/LINQ_Task/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int num = 0;
+            while (num <= 0)
+            {
+                Console.Write("Input the number: ");
+                if (!int.TryParse(Console.ReadLine(), out num))
+                {
+                    Console.WriteLine("Input is not valid for int data type. \nTry again!");
+                    if (num <= 0)
+                        Console.WriteLine("Input value must be more than zero. \nTry again!");
+                }
+            }
+            Console.WriteLine();
+
+            IEnumerable<int> numbers = Enumerable.Range(1, num);
+
+            // Factorial
+            foreach (int n in numbers)
+            {
+                Console.WriteLine($"{n}! = {numbers.SkipLast(num - n).Aggregate((x, y) => x * y)}");
+            }
+            Console.WriteLine();
+
+            // Fibonacci
+            foreach (int n in numbers)
+            {
+                int fibonacci = numbers.SkipLast(num - n + 1).Aggregate(
+                    new { previous = 0, current = 1 }, (anonList, _) =>
+                    new { previous = anonList.current, current = anonList.previous + anonList.current }
+                ).current;
+                Console.WriteLine($"The {n}-value of Fibonacci sequence is equal to {fibonacci}");
+            }
         }
     }
 }
